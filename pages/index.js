0c685b5becccf5500921347ef55d5f0abb53e464
Dinah2Grace <dinahgrace123@gmail.com>
2022-04-09 21:12:50 +0300

    prices

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,18 @@ import CardWarpper from '../components/cardwarpper';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import caps from '../images/caps.jpg';
-import Tshirt from '../images/tshirts.jpg';
-import hoodies from '../images/hoodie.jpg';
+import Tshirt from '../images/shirty.jpg';
+import hoodies from '../images/hooda.jpg';
 import Trouser from '../images/trouser.jpg';
 import Hoodie from '../images/hoodk.jpg';
 import Link from 'next/link';
 import Image from 'next/image';
 import Product from '../components/product';
 import capb from '../images/shhirt.jpg';
-
+import ant from '../images/shirto.jpg';
+import cap from '../images/capf.jpg';
+import capz from '../images/capi.jpg';
+import Shirt from '../images/shirrt.jpg';
 export default function Home() {
   return (
     <div >
@@ -24,7 +27,7 @@ export default function Home() {
           <div className='head-section'>
             {/* <span>Johnz Wear</span> */}
             <h1>
-              Find All Your Amazing Style Here.
+              Find All Your Amazing Styles Here.
             </h1>
             <Link href='/products'>
               Shop
@@ -38,8 +41,8 @@ export default function Home() {
           <div className='best-sellers-products'>
             <Product images={capb}/>
             <Product images={Hoodie}/>
-            <Product images={Hoodie}/>
-            <Product images={Hoodie}/>
+            <Product images={ant}/>
+            <Product images={cap}/>
           </div>
           <div className='best-seller-view-all'>
             <Link href='/products'>
@@ -50,7 +53,7 @@ export default function Home() {
       <div className='cartegories-section'>
         <div className='cartegory-row'>
             <div className='cart'>
-                <Image src={caps} width='640' height='400' />
+                <Image src={capz} width='640' height='400' />
                 <div className='cart-actions'>
                   <span>
                     Caps
@@ -62,7 +65,7 @@ export default function Home() {
             </div>
 
             <div className='cart'>
-                <Image src={hoodies} width='640' height='400' />
+                <Image src={Shirt} width='640' height='400' />
                 <div className='cart-actions'>
                   <span>
                     Caps
@@ -88,7 +91,7 @@ export default function Home() {
             </div>
 
             <div className='cart'>
-                <Image src={Trouser} width='640' height='400' />
+                <Image src={hoodies} width='640' height='400' />
                 <div className='cart-actions'>
                   <span>
                     Caps
