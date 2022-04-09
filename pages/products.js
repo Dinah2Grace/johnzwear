8348e2699333102ac9644251c8a7ba0c04d52387
Dinah2Grace <dinahgrace123@gmail.com>
2022-04-09 20:30:13 +0300

    latest

--- pages/products.js
+++ pages/products.js
@@ -9,6 +9,15 @@ import Productview from '../components/productview';
 import Product from '../components/product';
 // import capb from '../images/pexels-nathan-j-hilton-5343089.jpg'
 import capb from '../images/shhirt.jpg';
+import shirt from '../images/shirtq.jpg';
+import hoody from '../images/hoodk.jpg';
+import capy from '../images/capf.jpg';
+import shirty from '../images/shirto.jpg';
+import shirrt from '../images/shirteleven.jpg';
+import shsty from '../images/hooda.jpg';
+import cappy from '../images/cape.jpg';
+
+
 function Products() {
   return (
     <div>
@@ -23,14 +32,14 @@ function Products() {
             <div className='product_row'>
                 <Product images={capb}/>
                 <Product images={tshirt}/>
-                <Product images={capb}/>
-                <Product images={capb}/>
+                <Product images={hoody}/>
+                <Product images={capy}/>
             </div>
             <div className='product_row'>
-                <Product images={capb}/>
-                <Product images={capb}/>
-                <Product images={capb}/>
-                <Product images={capb}/>
+                <Product images={shirty}/>
+                <Product images={shsty}/>
+                <Product images={cappy}/>
+                <Product images={shirrt}/>
             </div>
             <div className='product_row'>
                 <Product images={capb}/>
